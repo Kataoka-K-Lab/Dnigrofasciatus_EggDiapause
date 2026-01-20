e169cf10ba0320ab7604fa3d86226cafccf0cf13
Kosuke Kataoka <kosuke.kataoka.lab@gmail.com>
2026-01-20 13:21:36 +0900

    Create ph.sh

---
+++ purge_haplotigs/ph.sh
@@ -0,0 +1,11 @@
+GENOME_FASTA=/Volumes/volume1/crickets/madarasuzu/masurca405/primary.genome.scf.fasta
+#READS=/Users/kosukekataoka/work1/tagame_genome/data/passed_all.fastq.gz
+BAM=/Volumes/volume1/crickets/madarasuzu/purgehaplotigs/out_sorted.bam
+THREADS=24
+
+purge_haplotigs hist \
+-b $BAM \
+-g $GENOME_FASTA \
+-t $THREADS
+
+ph.sh 1> ph.stdout 2> ph.stderr &
