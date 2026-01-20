d9855edef6ffa2b5ef1c1de8d9e7f0f5fcab873c
Kosuke Kataoka <kosuke.kataoka.lab@gmail.com>
2026-01-20 15:50:27 +0900

    Create plot.R

---
+++ mfuzz/plot.R
@@ -0,0 +1,3 @@
+pdf("/Users/kataokak/Desktop/mfuzz.plot.long.pdf")
+mfuzz.plot(long_avg_combined_s, cl = long_avg_combined_s_cl, mfrow=c(4, 5), time.labels = c(12, 24, 40, 56, 72), new.window = F)
+dev.off()
