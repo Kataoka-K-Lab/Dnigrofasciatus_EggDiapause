4c7f620300973ffa5ee90c52468a8990f28649cb
Kosuke Kataoka <kosuke.kataoka.lab@gmail.com>
2026-01-20 14:16:10 +0900

    Create repeatlandscape.sh

---
+++ repeat_analysis/repeatlandscape.sh
@@ -0,0 +1,42 @@
+ALIGN_IN=/work2/kataoka/madarasuzu/repeatmasker/masurca405_ph_hirise.fa.align
+FASTA_IN=/work2/kataoka/madarasuzu/hirise/masurca405_ph_hirise.fa
+SPECIES_NAME=Dianemobius_nigrofasciatus
+
+## Repeat Landscape ##
+
+### STEP 1 ###
+
+/home/kataoka/work/RepeatMasker/util/calcDivergenceFromAlign.pl \
+-s ${SPECIES_NAME}.divsum \
+${ALIGN_IN}
+
+### STEP 2 ###
+
+# エラーはいたがこれをやりおしたら以下うまくいった
+faToTwoBit ${FASTA_IN} ${SPECIES_NAME}.2bit
+
+### STEP 3 ###
+
+/home/kataoka/work/RepeatMasker/util/createRepeatLandscape.pl \
+-div ${SPECIES_NAME}.divsum \
+-twoBit ${SPECIES_NAME}.2bit > ${SPECIES_NAME}.html
+
+/home/kataoka/work/RepeatMasker/util/createRepeatLandscape.pl \
+-div Dianemobius_nigrofasciatus.divsum \
+-twoBit Dianemobius_nigrofasciatus.2bit > Dianemobius_nigrofasciatus.html
+
+## HTML File Analysis ##
+
+git clone https://github.com/kataksk/repeatlandscape_parser.git
+
+### STEP 4 ###
+
+python repeatlandscape_parser/repeatlandscape_html_to_csv.py \
+-s ${SPECIES_NAME} \
+-l ${SPECIES_NAME}.html --ggplot
+
+### STEP 5 ###
+
+python repeatlandscape_parser/repeatlandscape_html_pie_to_csv.py \
+-s ${SPECIES_NAME} \
+-l ${SPECIES_NAME}.html
